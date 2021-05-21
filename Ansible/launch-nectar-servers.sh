56fea0af85f180fa5f16c4c0ce5224ad05bb9b7f
Yuxuan Liu <patrickliuyx@gmail.com>
2021-05-21 14:12:37 +0800

    delete file

--- Ansible/launch-nectar-servers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-. ./CCC-grp-30-openrc.sh; ansible-playbook --ask-become-pass launch_nectar.yaml
--- Ansible/set_environment.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-. ./CCC-grp-30-openrc.sh; ansible-playbook --ask-become-pass set_environment.yaml -i inventory/hosts.ini 
