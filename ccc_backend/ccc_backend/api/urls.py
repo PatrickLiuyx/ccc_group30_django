7256f1e0e9d63025a58299216bf752d619a38ffc
Yuxuan Liu <patrickliuyx@gmail.com>
2021-05-25 23:42:58 +0800

    Django update debug

--- ccc_backend/ccc_backend/api/urls.py
+++ ccc_backend/ccc_backend/api/urls.py
@@ -8,28 +8,11 @@ from django.urls import path, re_path
 
 urlpatterns = []
 
-# url: http://127.0.0.1:8000/api/{urlpatterns}
-# e.g. http:localhost:8000/api/map/Map_bySuburb_March
-
-urlpatterns = [path('main/emotion.html/', multi_chart_emotion),]
+urlpatterns = [path('main/emotion.html/', multi_chart_emotion)]
 
 urlpatterns += [path('main/social_impact.html', multi_chart_social)]
 
-# show static file
-# urlpatterns += [
-#     re_path(r'^wordcloud_current/$', WC_current, name='wc'),
-# ]
-# urlpatterns += static('/wordcloud_current/', document_root=settings.MEDIA_ROOT)
-#
-# urlpatterns += [
-#     re_path(r'^wordcloud_previous/$', WC_previous, name='wc'),
-# ]
-# urlpatterns += static('/wordcloud_previous/', document_root=settings.MEDIA_ROOT)
-#
-# urlpatterns += [
-#     re_path(r'^wordcloud_before_the_previous/$', WC_before_the_previous, name='wc'),
-# ]
-# urlpatterns += static('/wordcloud_before_the_previous/', document_root=settings.MEDIA_ROOT)
+urlpatterns += [path('main/timeline.html', multi_chart_timeline)]
 
 urlpatterns += [path('main/', main_page)]
 
