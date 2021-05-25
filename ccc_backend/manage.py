3558ac39f7a6edc769778526ac2e4c7b391d9067
Hieler <51901593+Hieler@users.noreply.github.com>
2021-05-25 15:30:59 +0800

    Update manage.py

--- ccc_backend/manage.py
+++ ccc_backend/manage.py
@@ -2,6 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from streaming.harvestest import *
+from ccc_backend.settings import *
+from streaming.SentimentAnalysis import *
+from streaming.TextAnalytics import *
 
 root_dir_name = "ccc_backend"
 dir_path = os.path.abspath(sys.argv[0])
@@ -21,7 +25,12 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
+consumer_key = "7PC3L2O8ywCONU4ZuJ5h757mv"
+consumer_secret = "MMhJhIIolKU3ONdXpLC6oGzOwYGYwYgzMILc3etxZ0yjuNtdze"
+access_key = "1115405592372187136-19lxDiitrBBDSdSaLdyqgK3fJzJxWT"
+access_secret = "w2FsaXLITsGDy1PsRvqkdUdr78sKa6HYGcekcrB1p0xbM"
 if __name__ == '__main__':
-    # yilin
+
+    Fun_start(consumer_key, consumer_secret, access_key, access_secret)
     main()
+
